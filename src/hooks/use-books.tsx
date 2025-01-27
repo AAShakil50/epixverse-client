46783cce1ccf7558455c93a075e5ea9becbbd567
AAShakil50 <aashakil50@gmail.com>
2025-01-26 19:14:34 -0500

    fix: update useBooks hook and make projectID nullable

--- src/hooks/use-books.tsx
+++ src/hooks/use-books.tsx
@@ -6,12 +6,12 @@ import { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import useSWR from "swr";
 
-export function useBooks( projectID : string) {
+export function useBooks( projectID : string | null) {
     const [books, setBooks] = useRecoilState(booksAtom)
     const [activeBook, setActiveBook] = useRecoilState(activeBookAtom)
 
     const booksApiUrl = `${API_URL}/books?projectID=${projectID}`;
-    const { isLoading, error } = useSWR<Book[]>(booksApiUrl,
+    const { isLoading, error } = useSWR<Book[]>(projectID ? booksApiUrl : null,
         fetcher, {
         fallbackData: [],
         onSuccess(booksData) {
