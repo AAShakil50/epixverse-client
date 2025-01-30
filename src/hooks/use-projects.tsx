8327b8443fa83cc7550522b225b985d622e53da8
AAShakil50 <aashakil50@gmail.com>
2025-01-30 17:20:01 -0500

    feat: add useProjectOne hook to fetch project, books, chapters, and scenes

--- src/hooks/use-projects.tsx
+++ src/hooks/use-projects.tsx
@@ -1,7 +1,11 @@
 import { fetcher } from "@/lib/fetcher";
 import { API_URL } from "@/lib/site.configs";
 import { activeProjectAtom, projectsAtom } from "@/recoil/atoms/atom-projects";
+import { Book } from "@/types/book";
+import { Chapter } from "@/types/chapter";
 import { Project } from "@/types/project";
+import { Scene } from "@/types/scene";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import useSwr from "swr"
 
@@ -49,4 +53,82 @@ export function useProjects() {
         isLoading,
         error
     };
+}
+
+export function useProjectOne(projectId: string) {
+    // Fetch project
+    const { data: project, isLoading: isProjectLoading } = useSwr<Project[]>(
+        `${API_URL}/projects?id=${projectId}`,
+        fetcher,
+    );
+
+    // Fetch books for this project
+    const { data: books, isLoading: isBooksLoading } = useSwr<Book[]>(
+        `${API_URL}/books?projectID=${projectId}`,
+        fetcher,
+    );
+
+    const [chapters, setChapters] = useState<Chapter[] | null>(null);
+    const [isChaptersLoading, setIsChaptersLoading] = useState(true);
+
+    // Fetch chapters for all books (individual requests per book)
+    useEffect(() => {
+        const fetchChapters = async () => {
+            const chaptersResponses = await Promise.all<Chapter[] | null>(
+                books!.map((book) => fetcher<Chapter[] | null>(
+                    `${API_URL}/chapters?bookID=${book.id}`
+                ))
+            );
+
+            const validResponses = chaptersResponses.filter(
+                (response) => response != null
+            );
+
+            const finalChapters = validResponses.flat();
+
+            setChapters(finalChapters)
+            setIsChaptersLoading(false);
+        }
+
+        if (books) {
+            fetchChapters();
+            setIsChaptersLoading(true);
+        }
+    }, [books]);
+
+    const [scenes, setScenes] = useState<Scene[] | null>(null);
+    const [isScenesLoading, setIsScenesLoading] = useState(true);
+
+    // Fetch chapters for all books (individual requests per book)
+    useEffect(() => {
+        const fetchScenes = async () => {
+            const scenesResponses = await Promise.all<Scene[] | null>(
+                chapters!.map((chapter) => fetcher<Scene[] | null>(
+                    `${API_URL}/scenes?chapterID=${chapter.id}`
+                ))
+            );
+
+            const validResponses = scenesResponses.filter(
+                (response) => response != null
+            );
+
+            const finalScenes = validResponses.flat();
+
+            setScenes(finalScenes);
+            setIsScenesLoading(false);
+        }
+
+        if (chapters) {
+            fetchScenes()
+            setIsScenesLoading(true);
+        }
+    }, [chapters]);
+
+    return {
+        isLoading: isProjectLoading || isBooksLoading || isChaptersLoading || isScenesLoading,
+        project: project && project.length ? project[0] : null,
+        books: books,
+        chapters: chapters,
+        scenes: scenes
+    }
 }
