0a0c1bf10d336f45f3eaf4da5b8d8359e9f09c74
AAShakil50 <aashakil50@gmail.com>
2025-02-02 23:07:02 -0500

    feat: add GraphQL queries and types for project management

---
+++ src/graphql/generated/types.ts
@@ -0,0 +1,227 @@
+import { gql } from '@apollo/client';
+import * as Apollo from '@apollo/client';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type MakeEmpty<T extends { [key: string]: unknown }, K extends keyof T> = { [_ in K]?: never };
+export type Incremental<T> = T | { [P in keyof T]?: P extends ' $fragmentName' | '__typename' ? T[P] : never };
+const defaultOptions = {} as const;
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: { input: string; output: string; }
+  String: { input: string; output: string; }
+  Boolean: { input: boolean; output: boolean; }
+  Int: { input: number; output: number; }
+  Float: { input: number; output: number; }
+};
+
+export type Book = {
+  __typename?: 'Book';
+  chapters?: Maybe<Array<Chapter>>;
+  description?: Maybe<Scalars['String']['output']>;
+  id: Scalars['ID']['output'];
+  title: Scalars['String']['output'];
+};
+
+
+export type BookChaptersArgs = {
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+};
+
+export type Chapter = {
+  __typename?: 'Chapter';
+  id: Scalars['ID']['output'];
+  scenes?: Maybe<Array<Scene>>;
+  title: Scalars['String']['output'];
+};
+
+
+export type ChapterScenesArgs = {
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+};
+
+export type Elemental = {
+  __typename?: 'Elemental';
+  description?: Maybe<Scalars['String']['output']>;
+  elemental?: Maybe<Elemental>;
+  id: Scalars['ID']['output'];
+  title: Scalars['String']['output'];
+};
+
+export type EpixVerse = {
+  __typename?: 'EpixVerse';
+  app: Scalars['String']['output'];
+  description: Scalars['String']['output'];
+  developer?: Maybe<Array<Scalars['String']['output']>>;
+  version: Scalars['String']['output'];
+};
+
+export type Project = {
+  __typename?: 'Project';
+  books?: Maybe<Array<Book>>;
+  description?: Maybe<Scalars['String']['output']>;
+  elementals?: Maybe<Array<Elemental>>;
+  id: Scalars['ID']['output'];
+  title: Scalars['String']['output'];
+};
+
+
+export type ProjectBooksArgs = {
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+};
+
+
+export type ProjectElementalsArgs = {
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+};
+
+export type Query = {
+  __typename?: 'Query';
+  book?: Maybe<Book>;
+  books: Array<Book>;
+  chapter?: Maybe<Chapter>;
+  chapters: Array<Chapter>;
+  elemental?: Maybe<Elemental>;
+  elementals: Array<Elemental>;
+  epixverse: EpixVerse;
+  project?: Maybe<Project>;
+  projects: Array<Project>;
+  scene?: Maybe<Scene>;
+  scenes: Array<Scene>;
+};
+
+
+export type QueryBookArgs = {
+  id: Scalars['ID']['input'];
+};
+
+
+export type QueryBooksArgs = {
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+};
+
+
+export type QueryChapterArgs = {
+  id: Scalars['ID']['input'];
+};
+
+
+export type QueryChaptersArgs = {
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+};
+
+
+export type QueryElementalArgs = {
+  id: Scalars['ID']['input'];
+};
+
+
+export type QueryElementalsArgs = {
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+  projectID?: InputMaybe<Scalars['ID']['input']>;
+};
+
+
+export type QueryProjectArgs = {
+  id: Scalars['ID']['input'];
+};
+
+
+export type QueryProjectsArgs = {
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+};
+
+
+export type QuerySceneArgs = {
+  id: Scalars['ID']['input'];
+};
+
+
+export type QueryScenesArgs = {
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+};
+
+export type Scene = {
+  __typename?: 'Scene';
+  description?: Maybe<Scalars['String']['output']>;
+  id: Scalars['ID']['output'];
+  index: Scalars['Int']['output'];
+  title?: Maybe<Scalars['String']['output']>;
+};
+
+export type GetProjectsQueryVariables = Exact<{
+  limit?: InputMaybe<Scalars['Int']['input']>;
+  offset?: InputMaybe<Scalars['Int']['input']>;
+}>;
+
+
+export type GetProjectsQuery = { __typename?: 'Query', projects: Array<{ __typename?: 'Project', id: string, title: string, description?: string | null, books?: Array<{ __typename?: 'Book', id: string, title: string, chapters?: Array<{ __typename?: 'Chapter', id: string, title: string, scenes?: Array<{ __typename?: 'Scene', id: string, title?: string | null, index: number }> | null }> | null }> | null }> };
+
+
+export const GetProjectsDocument = gql`
+    query GetProjects($limit: Int, $offset: Int) {
+  projects(limit: $limit, offset: $offset) {
+    id
+    title
+    description
+    books {
+      id
+      title
+      chapters {
+        id
+        title
+        scenes {
+          id
+          title
+          index
+        }
+      }
+    }
+  }
+}
+    `;
+
+/**
+ * __useGetProjectsQuery__
+ *
+ * To run a query within a React component, call `useGetProjectsQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetProjectsQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetProjectsQuery({
+ *   variables: {
+ *      limit: // value for 'limit'
+ *      offset: // value for 'offset'
+ *   },
+ * });
+ */
+export function useGetProjectsQuery(baseOptions?: Apollo.QueryHookOptions<GetProjectsQuery, GetProjectsQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetProjectsQuery, GetProjectsQueryVariables>(GetProjectsDocument, options);
+      }
+export function useGetProjectsLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetProjectsQuery, GetProjectsQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetProjectsQuery, GetProjectsQueryVariables>(GetProjectsDocument, options);
+        }
+export function useGetProjectsSuspenseQuery(baseOptions?: Apollo.SkipToken | Apollo.SuspenseQueryHookOptions<GetProjectsQuery, GetProjectsQueryVariables>) {
+          const options = baseOptions === Apollo.skipToken ? baseOptions : {...defaultOptions, ...baseOptions}
+          return Apollo.useSuspenseQuery<GetProjectsQuery, GetProjectsQueryVariables>(GetProjectsDocument, options);
+        }
+export type GetProjectsQueryHookResult = ReturnType<typeof useGetProjectsQuery>;
+export type GetProjectsLazyQueryHookResult = ReturnType<typeof useGetProjectsLazyQuery>;
+export type GetProjectsSuspenseQueryHookResult = ReturnType<typeof useGetProjectsSuspenseQuery>;
+export type GetProjectsQueryResult = Apollo.QueryResult<GetProjectsQuery, GetProjectsQueryVariables>;
