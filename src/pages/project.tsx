ea8c6280302b5a01e9e433dd081788bd58420fa0
AAShakil50 <aashakil50@gmail.com>
2025-02-05 16:41:52 -0500

    feat: add unique key prop to SectionBook component for improved rendering

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -129,6 +129,7 @@ const SectionBooks = ({ books }: { books: Book[] | null | undefined }) => {
     grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
         {
             books.map((book) => <SectionBook
+                key={book.id}
                 book={book} />)
         }
     </section>
