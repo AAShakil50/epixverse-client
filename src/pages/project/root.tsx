4ae5b70c6056d1c0636d7297998ebd0ff15a7a87
AAShakil50 <aashakil50@gmail.com>
2025-03-01 01:35:24 -0500

    feat: enhance SectionTable in ProjectIndex to include expand links for books

--- src/pages/project/root.tsx
+++ src/pages/project/root.tsx
@@ -1,7 +1,8 @@
-import { useSearchParams } from "react-router-dom";
-import { SectionTable } from "../project";
-import { useGetProjectQuery } from "@/graphql/generated/types";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useGetProjectQuery } from "@/graphql/generated/types";
+import { ChevronsRight } from "lucide-react";
+import { Link, useSearchParams } from "react-router-dom";
+import { SectionTable } from "../project";
 
 const ProjectIndex = () => {
   const [params] = useSearchParams();
@@ -21,11 +22,18 @@ const ProjectIndex = () => {
   ) : (
     <SectionTable
       caption="List of Books"
-      headings={["Title", "Description", "Items"]}
-      rows={[
-        data.project.books?.map((item) => item.title) ?? [],
-        data.project.books?.map((item) => item.description ?? "") ?? [],
-      ]}
+      headings={["Title", "Description", "Expand"]}
+      rows={
+        data.project.books?.map((item) => {
+          return [
+            <span key={item.id}>{item.title}</span>,
+            <span key={item.id}>{item.description}</span>,
+            <Link key={item.id} to={`/project/book?id=${item.id}`}>
+              <ChevronsRight key={item.id} />
+            </Link>,
+          ];
+        }) ?? []
+      }
     />
   );
 };
