7b8e0780bef736a834dc1e1d5b4c17724bc6b05f
AAShakil50 <aashakil50@gmail.com>
2025-03-01 02:04:49 -0500

    feat: implement ProjectBook component with loading state and navigation links

--- src/pages/project/book.tsx
+++ src/pages/project/book.tsx
@@ -1,12 +1,49 @@
-import { SectionTable } from "../project";
+import { Skeleton } from "@/components/ui/skeleton";
+import { ChevronLeft, ChevronsRight } from "lucide-react";
+import { useSearchParams, Link } from "react-router-dom";
+import { SectionTable } from "@/pages/project";
+import { useProjectByBookID } from "@/hooks/use-projects";
 
 const ProjectBook = () => {
-  return (
-    <SectionTable
-      caption="List of Chapters"
-      headings={["Title", "Description", "Items"]}
-      rows={[]}
-    />
+  const [params] = useSearchParams();
+  const resId = params.get("id");
+
+  const { data: project, loading } = useProjectByBookID(resId);
+
+  return loading ? (
+    <Skeleton />
+  ) : !project?.books ? (
+    <span>Not found</span>
+  ) : (
+    <>
+      <div
+        className="my-8 mx-8 josefin-sans
+                flex flex-row "
+      >
+        <ChevronLeft />
+        <Link to={`/project?id=${project.id}`}>
+          <span className="underline text-lg cursor-pointer">
+            {project.title}
+          </span>
+        </Link>
+      </div>
+      <SectionTable
+        caption="List of Books"
+        headings={["Title", "Expand"]}
+        rows={
+          project.books
+            .find((item) => item.id === resId)
+            ?.chapters?.map((item) => {
+              return [
+                <b key={item.id}>{item.title}</b>,
+                <Link key={item.id} to={`/project/chapter?id=${item.id}`}>
+                  <ChevronsRight key={item.id} />
+                </Link>,
+              ];
+            }) ?? []
+        }
+      />
+    </>
   );
 };
 
